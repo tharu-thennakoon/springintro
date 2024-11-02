ce69f8a29d09bb8c8a3919d650cb848508a3f2ff
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-02 17:57:15 +0530

    Add another service for TaskService

--- springintro/src/main/java/com/ijse/springintro/service/TaskService.java
+++ springintro/src/main/java/com/ijse/springintro/service/TaskService.java
@@ -11,8 +11,5 @@ import com.ijse.springintro.Entity.Task;
 public interface TaskService {
     List<Task> getTasksList(); //reading
     Task createTask(Task task);  //creating
-
-    
-
-
+    Task geTaskByID(Long id);  //finding a task by it's primary key
 }
