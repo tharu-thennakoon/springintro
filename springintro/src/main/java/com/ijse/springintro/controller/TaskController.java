77be8a0c138d4b75d11215de244edaadc1eef6e5
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-03 11:09:50 +0530

    create API endpoint(delete) and finished the crud

--- springintro/src/main/java/com/ijse/springintro/controller/TaskController.java
+++ springintro/src/main/java/com/ijse/springintro/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.ijse.springintro.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,6 +71,11 @@ public class TaskController {
         else{
             return ResponseEntity.status(200).body(updateTask);
         }
+    }
+
+    @DeleteMapping("/task/{taskId}")
+    public void deleteTask(@PathVariable Long taskId){
+        taskService.deleteTask(taskId);
     }
 
 }
