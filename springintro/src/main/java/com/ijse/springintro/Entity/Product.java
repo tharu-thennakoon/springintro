eed8fd85c9c2a76bcfc83fecd1ce3d6f804fe158
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-04 12:39:31 +0530

    create attributes for product entity

--- springintro/src/main/java/com/ijse/springintro/Entity/Product.java
+++ springintro/src/main/java/com/ijse/springintro/Entity/Product.java
@@ -1,8 +1,24 @@
 package com.ijse.springintro.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Product {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private Double price;
+    private String description;
 }
