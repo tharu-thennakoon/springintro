e281aec24457fd5c6e6b92f22652f91b082b248c
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-17 21:58:26 +0530

    create endpoint for category view

--- springintro/src/main/java/com/ijse/springintro/controller/CategoryController.java
+++ springintro/src/main/java/com/ijse/springintro/controller/CategoryController.java
@@ -1,5 +1,26 @@
 package com.ijse.springintro.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ijse.springintro.Entity.Category;
+import com.ijse.springintro.service.CategoryService;
+
+@RestController
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getCategories(){
+        List<Category> categories = categoryService.getAllCategories(); 
+        return ResponseEntity.status(200).body(categories);
+        
+    }
     
 }
