fe470df06a973182347f5ada2bef3f87a0777f34
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-23 22:59:37 +0530

    Add Post Mapping

--- springintro/src/main/java/com/ijse/springintro/controller/CategoryController.java
+++ springintro/src/main/java/com/ijse/springintro/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ijse.springintro.Entity.Category;
@@ -24,7 +25,7 @@ public class CategoryController {
     }
 
     @PostMapping("/categories")
-    public ResponseEntity<Category> createCategory(Category category){
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
         Category createCategory = categoryService.createCategory(category);
 
         return ResponseEntity.status(201).body(createCategory);
