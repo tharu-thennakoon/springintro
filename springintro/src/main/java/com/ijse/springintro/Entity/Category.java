4c5f7e4c09c706f52ea055a91462e78fd288548c
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-24 16:07:46 +0530

    fix the show product and category error

--- springintro/src/main/java/com/ijse/springintro/Entity/Category.java
+++ springintro/src/main/java/com/ijse/springintro/Entity/Category.java
@@ -2,6 +2,8 @@ package com.ijse.springintro.Entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ public class Category {
 
    private String name; 
 
+   @JsonIgnore
    @OneToMany(mappedBy = "category")
    private List<Product> products;   //return type will be Array
 }
