67bf0e4f6e05a55aa2706d8385089b4751a3531e
tharu-thennakoon <tharushikalpani2003@gmail.com>
2024-11-26 16:05:16 +0530

    create productReqDTO for add product easily using only category ID

---
+++ springintro/src/main/java/com/ijse/springintro/dto/ProductReqDTO.java
@@ -0,0 +1,15 @@
+// add product easily using only category ID
+
+package com.ijse.springintro.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductReqDTO {
+    private String name;
+    private Double price;
+    private String description;
+    private Long categoryId;
+}
